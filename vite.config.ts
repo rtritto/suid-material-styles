4a057e4e34af572c9d7c4e7ba81c404a24b08940
rtritto <r.tritto5@studenti.uniba.it>
2023-05-08 22:23:53 +0200

    Add @suid/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import suid from '@suid/vite-plugin'
 import solid from 'vite-plugin-solid'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [solid({ ssr: true }), ssr()]
+  plugins: [suid(), solid({ ssr: true }), ssr()]
 }
 
 export default config
